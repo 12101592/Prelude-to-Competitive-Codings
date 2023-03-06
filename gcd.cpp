c6cf676b0f2453617f75dce387ef8e72f2d0ad37
Mohd Afzal Raza <94395127+12101592@users.noreply.github.com>
2023-03-06 14:35:37 +0530

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+ using namespace std;
+  int main(){
+  	
+  	int n,m;
+  	 cout<<"Enter the first number: ";
+  	 cin>>n;
+  	 cout<<"Enter the Second number: ";
+  	 cin>>m;
+  	 int gcd= __gcd(m,n);
+  	 
+  	 
+  	 cout<<gcd;
+  	  return 0;
+  	  
+  }
+
